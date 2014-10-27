e286082de21d7e3198964cb519fbf4a3a6a27a65
Jimmy Huang <pake007@gmail.com>
2014-10-27 08:21:49 +0800

    secrets yml in production

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,8 @@ set :user,            'pake'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
+set :linked_files, %w{config/secrets.yml}
+
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
